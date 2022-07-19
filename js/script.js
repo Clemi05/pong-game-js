28a1bd91b497e6a6ca95b3585cd801a7d9b4a01a
Clement Azalbert <clementazalbert04@gmail.com>
2022-07-19 16:30:17 +0200

    added a hue variable in function update(time) from script.js in order to change the color of the game while playing

--- js/script.js
+++ js/script.js
@@ -14,8 +14,12 @@ let lastTime;
 function update(time) {
   if (lastTime != null) {
     const delta = time - lastTime;
-    // ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
+    ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
     computerPaddle.update(delta, ball.y);
+
+    // Changes the color of the game while playing
+    const hue = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--hue"));
+    document.documentElement.style.setProperty("--hue", hue + delta * 0.01);
 
     if (isLose()) handleLose();
   }
